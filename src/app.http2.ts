47f2c2511437cc6a6262b69905e25bf3d281f902
Jesús <jesusguerreropaniagua6@gmail.com>
2024-05-23 21:32:35 +0200

    feat: first commit

---
+++ src/app.http2.ts
@@ -0,0 +1,35 @@
+import http2 from "http2";
+import fs from "fs";
+
+const server = http2.createSecureServer(
+  {
+    key: fs.readFileSync("./keys/server.key"),
+    cert: fs.readFileSync("./keys/server.crt"),
+  },
+  (req, res) => {
+    if (req.url === "/") {
+      const htmlFile = fs.readFileSync("./public/index.html", "utf-8");
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(htmlFile);
+      return;
+    }
+
+    if (req.url?.endsWith(".js")) {
+      res.writeHead(200, { "Content-Type": "text/javascript" });
+    } else if (req.url?.endsWith(".css")) {
+      res.writeHead(200, { "Content-Type": "text/css" });
+    }
+
+    try {
+      const responseContent = fs.readFileSync(`./public${req.url}`, "utf-8");
+      res.end(responseContent);
+    } catch {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end();
+    }
+  }
+);
+
+server.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
