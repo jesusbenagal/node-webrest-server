44ffc274e88edb52662c8a94a8d65a437809438d
Jesús <jesusguerreropaniagua6@gmail.com>
2024-05-28 23:51:12 +0200

    feat: installed compression package

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,4 +1,5 @@
 import express, { Router } from "express";
+import compression from "compression";
 import path from "path";
 
 interface Options {
@@ -25,6 +26,7 @@ export class Server {
     //* Middlewares
     this.app.use(express.json()); // for parsing application/json
     this.app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+    this.app.use(compression());
 
     //* Public folders
     this.app.use(express.static(this.publicPath));
