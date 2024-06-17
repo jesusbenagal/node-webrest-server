169d826e1f763a47ff675f835789f166b45ac4f8
Jesús Guerrero <jesus.guerrero@talan.com>
2024-06-17 13:11:42 +0200

    feat: added tests

--- tests/presentation/todos/routes.test.ts
+++ tests/presentation/todos/routes.test.ts
@@ -12,11 +12,14 @@ describe("Todo routes tests", () => {
     testServer.close();
   });
 
+  beforeEach(async () => {
+    await prisma.todo.deleteMany();
+  });
+
   const todo1 = { text: "todo1" };
   const todo2 = { text: "todo2" };
 
   test("should return TODOs api/todos", async () => {
-    await prisma.todo.deleteMany();
     await prisma.todo.createMany({
       data: [todo1, todo2],
     });
@@ -30,5 +33,60 @@ describe("Todo routes tests", () => {
     expect(body[0].text).toBe(todo1.text);
     expect(body[1].text).toBe(todo2.text);
     expect(body[0].completedAt).toBe(null);
+  });
+
+  test("should return a TODO by id api/todos/:id", async () => {
+    const todo = await prisma.todo.create({ data: todo1 });
+
+    const { body } = await request(testServer.app)
+      .get(`/api/todos/${todo.id}`)
+      .expect(200);
+
+    expect(body).toEqual({
+      id: todo.id,
+      text: todo.text,
+      completedAt: todo.completedAt,
+    });
+  });
+
+  test("should return a 404 NotFound api/todos/:id", async () => {
+    const todoId = 999;
+
+    const { body } = await request(testServer.app)
+      .get(`/api/todos/${todoId}`)
+      .expect(400);
+
+    expect(body).toEqual({ error: "Todo not found" });
+  });
+
+  test("should return a new TODO api/todos", async () => {
+    const { body } = await request(testServer.app)
+      .post("/api/todos")
+      .send(todo1)
+      .expect(201);
+
+    expect(body).toEqual({
+      id: expect.any(Number),
+      text: todo1.text,
+      completedAt: null,
+    });
+  });
+
+  test("should return an Error if text is not present api/todos", async () => {
+    const { body } = await request(testServer.app)
+      .post("/api/todos")
+      .send({})
+      .expect(400);
+
+    expect(body).toEqual({ error: "Text is required" });
+  });
+
+  test("should return an Error if text is empty api/todos", async () => {
+    const { body } = await request(testServer.app)
+      .post("/api/todos")
+      .send({ text: "" })
+      .expect(400);
+
+    expect(body).toEqual({ error: "Text is required" });
   });
 });
